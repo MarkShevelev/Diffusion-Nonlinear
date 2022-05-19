56408e6fd85315126cbf54fde040f31e50288fe2
MarkShevelev <mposimba@gmail.com>
2022-05-19 16:56:46 +0300

    parameters are read from the standard input stream

--- research/main.cpp
+++ research/main.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <fstream>
 #include <iomanip>
+#include <string>
+#include <sstream>
 
 #include <sweep_matrix_calculation.hpp>
 #include <rha_calculation.hpp>
@@ -85,23 +87,63 @@ void dV_dI_initial(T *dV_dI, std::size_t size, T eps, T kappa, GetI<T> const &I)
     dV_dI[0] = dV_dI[size - 1] = static_cast<T>(0);
 }
 
+
+template <typename T>
+struct Parameters {
+    T eps, kappa;
+    unsigned grid_size;
+    T dt;
+    unsigned iter_size;
+};
+
+unsigned const MAX_GRID_SIZE = 10240;
+
 int main() {
-    std::size_t const size = 32;
+    Parameters<double> p;
+    std::cout << "Please, enter epsilon, kappa, grid-size, time-step and a-number-of-iterations: " << std::endl;
+    std::cin >> p.eps >> p.kappa >> p.grid_size >> p.dt >> p.iter_size;
+    if (!std::cin) {
+        std::cerr << "Can't read parameters from the standard input stream" << std::endl;
+        return 0;
+    }
+
+    if (p.eps < 0. || p.eps > 1. || p.kappa < 0. || p.kappa > 1.) {
+        std::cerr << "epsilon and kappa should be positive and less than 1" << std::endl;
+        return 0;
+    }
+
+    if (p.grid_size > MAX_GRID_SIZE) {
+        std::cerr << "Grid size is too large.\nPlease, enter a positive integer value less than " << MAX_GRID_SIZE << std::endl;
+        return 0;
+    }
+
+    if (p.dt < 0.) {
+        std::cerr << "Time step should be positive" << std::endl;
+        return 0;
+    }
+
+    std::size_t const size = p.grid_size;
+    std::size_t const iter_size = p.iter_size;
     std::vector<double> 
         f_curr(size), f_pred(size),
         Dc(size), V(size), dV_dI(size),
         F0_curr(size), F1_curr(size), F0_pred(size), F1_pred(size),
         a(size), b(size), c(size), d(size);
-    double dI = 2.f / (size - 1), dt = 1.0e-2f, rdI = 1.0f / dI, eps = 1.f, kappa = 0.0;
+    double dI = 2.f / (size - 1), dt = p.dt, rdI = 1.0f / dI, eps = p.eps, kappa = p.kappa;
     GetI<double> I(size);
-    std::cout << dI << " " << rdI << " " << dt << " " << kappa << " " << rdI * dt << " " << rdI * rdI * dt << std::endl;
+    std::cout 
+        << "dI = " << dI << "\n" 
+        << "1/dI = " << rdI << "\n"
+        << "dt/dI = " << rdI * dt << "\n" 
+        << "dt/(dI * dI)" << dt * rdI * rdI << "\n"
+        << std::endl;
 
     f_initial(f_curr.data(), size, (size - 1) * 2);
     Dc_initial(Dc.data(), size, eps, I);
     V_initial(V.data(), size, eps, kappa, I);
     dV_dI_initial(dV_dI.data(), size, eps, kappa, I);
 
-    for (unsigned iter_cnt = 0; iter_cnt != 300; ++iter_cnt) {
+    for (unsigned iter_cnt = 0; iter_cnt != iter_size; ++iter_cnt) {
         step(
             f_curr.data(), f_pred.data(),
             a.data(), b.data(), c.data(), d.data(),
@@ -112,7 +154,9 @@ int main() {
 
     // export
     {
-        std::ofstream fout("./data.txt");
+        std::stringstream output_filename;
+        output_filename << eps << '-' << kappa << '-' << size << '-' << dt << ".dat";
+        std::ofstream fout(output_filename.str());
         if (!fout)
             std::cerr << "Can't open file" << std::endl;
         else {
