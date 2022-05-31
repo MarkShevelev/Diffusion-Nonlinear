942d1670ebc9441e57e15de8ae40a3cf64115391
MarkShevelev <mposimba@gmail.com>
2022-05-31 21:24:50 +0300

    reversed to continuity based calculation

--- rha_calculation.hpp
+++ rha_calculation.hpp
@@ -5,10 +5,10 @@
 template<typename T>
 void F0_calculation(T *F0, T const *f, T const *dV_dI, std::size_t size) {
     for (std::size_t elm_idx = 1; elm_idx != size / 2; ++elm_idx)
-        F0[elm_idx] = static_cast<T>(0);
+        F0[elm_idx] = dV_dI[elm_idx] * f[elm_idx];
 
     for (std::size_t elm_idx = size / 2; elm_idx != size - 1; ++elm_idx) 
-        F0[elm_idx] = dV_dI[elm_idx] * (f[size - elm_idx - 1] - f[elm_idx]);
+        F0[elm_idx] = -F0[elm_idx]; 
 }
 
 template<typename T>
@@ -20,7 +20,7 @@ void F1_calculation(T *F1, T const *f, T const *Dc, T const *V, std::size_t size
                 Dc[elm_idx] * f[elm_idx + 1] - 
                 (Dc[elm_idx - 1] + Dc[elm_idx]) * f[elm_idx]
             )
-            - static_cast<T>(0.5) * rdI * V[elm_idx] * (
-                f[elm_idx + 1] - f[elm_idx - 1]
+            - static_cast<T>(0.5) * rdI * (
+                V[elm_idx + 1] * f[elm_idx + 1] - V[elm_idx - 1] * f[elm_idx - 1]
             );
 }
